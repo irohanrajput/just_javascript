5a0dbe8e38ea35052dba2a7cc1a4e8abdae44f61
irohanrajput <itusharsingh27@gmail.com>
2023-08-15 04:11:39 +0530

    pending task

--- js2023.08.10/sorting_array.js
+++ js2023.08.10/sorting_array.js
@@ -1,17 +1,19 @@
-function uniqueNsorted(array) {
-    array2 = [];
-    for (let i = 0; i < array.length; i++) {
-        if (array2.indexOf(array[i]) == -1){
-            array2.push(array[i])
-        }
-    }
+// function uniqueNsorted(array) {
+//     array2 = [];
+//     for (let i = 0; i < array.length; i++) {
+//         if (array2.indexOf(array[i]) == -1){
+//             array2.push(array[i])
+//         }
+//     }
 
-    for (let i = 0; i < array2.length; i++) {
-        if ()
-    }
-}
+//     for (let i = 0; i < array2.length; i++) {
+//         if ()
+//     }
+// }
 
-array = [1,2,3,2,4,4,4,4,4,4,6,3,5,7,1,2,5,6,4]
-console.log(unique(array))
+// array = [1,2,3,2,4,4,4,4,4,4,6,3,5,7,1,2,5,6,4]
+// console.log(unique(array))
 
-module.exports = unique;
+// module.exports = unique;
+
+
