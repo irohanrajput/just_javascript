04cb1bcd01d21470bbd5de594111203db158228e
irohanrajput <itusharsingh27@gmail.com>
2024-03-29 22:05:35 +0530

    done with the concepts, let's build something rn

---
+++ 3rd_Attempt_to_learn_JS/03_basics/04_IIFE.js
@@ -0,0 +1,2 @@
+// immediately invoked function expressions
+
---
+++ 3rd_Attempt_to_learn_JS/05_iterations/01_one.js
@@ -0,0 +1,7 @@
+for (let i = 0; i <= 10; i++) {
+    const element = i
+    if (element == 5) {
+        console.log("5 is the best number")
+    }
+      console.log(element)
+}
